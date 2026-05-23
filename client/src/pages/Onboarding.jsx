5b0a2c2c14c3a8bfa4cf467df62901fa26c0b4ee
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-05-23 20:40:01 +0100

    feat(client/onboarding): add layout for higher screen resolution for onboarding page

--- client/src/pages/Onboarding.jsx
+++ client/src/pages/Onboarding.jsx
@@ -2,23 +2,23 @@ function Onboarding() {
   return (
     <main className="p-2 flex flex-col h-screen">
       <header>
-        <h1 className="font-anton text-4xl text-center">Aura</h1>
+        <h1 className="font-anton text-4xl md:text-5xl text-center">Aura</h1>
       </header>
 
-      <div className="flex-1 flex flex-col items-center justify-center gap-5">
+      <div className="flex-1 flex flex-col items-center justify-center gap-6">
         <div className="flex flex-col text-center gap-1">
-          <p className="font-anton text-5xl">LESS NOISE.</p>
-          <p className="font-anton text-4xl">MORE CONVERSATION.</p>
-          <p className="font-semibold text-secondary">
+          <p className="font-anton text-5xl md:text-8xl">LESS NOISE.</p>
+          <p className="font-anton text-4xl md:text-6xl">MORE CONVERSATION.</p>
+          <p className="font-semibold text-secondary md:text-lg">
             Aura keeps messaging clean, fast, and distraction-free.
           </p>
         </div>
 
         <div className="flex gap-4">
-          <button className="w-30 bg-primary-button text-primary-button-text text-lg font-semibold py-2 px-4 rounded-sm cursor-pointer">
+          <button className="w-30 bg-primary-button text-primary-button-text text-lg md:text-xl font-semibold py-2 px-4 rounded-sm cursor-pointer">
             Sign Up
           </button>
-          <button className="w-30 bg-primary-button text-primary-button-text text-lg font-semibold py-2 px-4 rounded-sm cursor-pointer">
+          <button className="w-30 bg-primary-button text-primary-button-text text-lg md:text-xl font-semibold py-2 px-4 rounded-sm cursor-pointer">
             Login
           </button>
         </div>
