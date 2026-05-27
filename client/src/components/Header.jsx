18222a4567186d16248d4fd3763c77bb46fb130c
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-05-27 12:00:41 +0100

    feat(client): add navigation to home route on click

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Menu } from "lucide-react";
+import { useNavigate } from "react-router";
 
 function Header({ userData, setSidebar }) {
+  const navigate = useNavigate();
+
   return (
     <header className="flex p-2 items-center md:bg-[hsl(30,20%,8%)]">
       <button
@@ -10,11 +13,17 @@ function Header({ userData, setSidebar }) {
         <Menu />
       </button>
       <div className="flex-1">
-        <h1 className="font-anton text-4xl md:text-5xl text-center md:text-start">
+        <h1
+          className="font-anton text-4xl md:text-5xl text-center md:text-start cursor-default"
+          onClick={() => navigate("/")}
+        >
           Aura
         </h1>
       </div>
-      <button className="font-bold bg-white/8 backdrop-blur-xl hover:bg-white/10 w-fit md:pr-2 rounded-full flex items-center gap-2 cursor-pointer">
+      <button
+        className="font-bold bg-white/8 backdrop-blur-xl hover:bg-white/10 w-fit md:pr-2 rounded-full flex items-center gap-2 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
         <div className="w-10 h-10 rounded-full bg-green-400 flex items-center justify-center">
           {userData.username.charAt(0)}
         </div>
